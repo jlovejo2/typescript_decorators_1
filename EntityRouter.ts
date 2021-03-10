509a6c8b58f2d0887a1e29e8eba36dd8cbbb95fb
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-10 16:19:11 -0600

    added some console logs to entityRouter.ts

--- EntityRouter.ts
+++ EntityRouter.ts
@@ -19,6 +19,7 @@ export default class EntityRouter<T extends BaseEntity> {
   addEntityRoutes() {
     //CREATE
     this._router.post('/', (req, res) => {
+      console.log('enter POST....')
       this.createEntity(req, res)
     })
 
@@ -56,11 +57,39 @@ export default class EntityRouter<T extends BaseEntity> {
   }
 
   private createEntity(req: Request, res: Response) {
-
+    // create new persistence obj based on what is passed in
+    let newEntity = EntityFactory.formPersistenceObject<T>(req.body, this.classRef);
+    // getting the id property 
+    const idProperty = Reflect.getMetadata("entity:id", newEntity);
+    // setting id value getting uuid
+    newEntity[idProperty] = uuid.v4();
+    // passing value into the database, with persistance object
+    console.log('newEntity: ', newEntity)
+    db.push(`/${this.name}/${newEntity[idProperty]}`, newEntity.getPersistenceObject());
+    res.status(200).json(newEntity)
   }
 
   private updateEntity(req: Request, res: Response) {
+    // does entity exist in db?
+    let data = {}
+    try {
+      data = db.getData(`${this.name}/${req.params.id}`)
+    } catch (err) {
+      res.status(404).json({error: "Object does not exist"})
+    }
 
+    // update the object
+    let updatedData = req.body
+    let updatedObj = EntityFactory.formPersistenceObject(data, this.classRef)
+    const propKeys = Object.keys(updatedData)
+    for (const propKey of propKeys) {
+      updatedObj[propKey] = updatedData[propKey]
+    }
+
+    //save and return data
+    db.push(`/${this.name}/${req.params.id}`, updatedData, false)
+    data = db.getData(`/${this.name}/${req.params.id}`)
+    res.json(data);
   }
 
   private deleteEntity(req: Request, res: Response) {

db292e2e249845326faa1b9bd6b71be5fe118696
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-11 05:39:06 -0600

    fixed console.log of req.hostname instead of req.host for log.ts decorator

--- decorators/log.ts
+++ decorators/log.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
+//method decorator (not a factory)
 export function logRoute(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
   //save original function
   const original = descriptor.value
@@ -9,7 +10,7 @@ export function logRoute(target: any, propertyKey: string, descriptor: PropertyD
     //call the original function with arguments that were passed into it
     original.apply(this, args);
     //log to console
-    console.log(`${req.ip} [${new Date().toISOString()}] ${req.host} ${req.originalUrl} ${req.method} ${res.statusCode} ${res.statusMessage} HTTP/${req.httpVersion}`);
+    console.log(`${req.ip} [${new Date().toISOString()}] ${req.hostname} ${req.originalUrl} ${req.method} ${res.statusCode} ${res.statusMessage} HTTP/${req.httpVersion}`);
     //if you are using PUT OR POST going to output body that was sent in
     if (["PUT", "POST"].indexOf(req.method) > -1) {
       console.log(`\tBODY: ${JSON.stringify(req.body)}`)

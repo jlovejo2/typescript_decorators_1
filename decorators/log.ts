2f6552246d9dd2a4f00378285caa70f232ed3121
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-11 05:27:37 -0600

    created log.ts decorator for logging user in

---
+++ decorators/log.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+export function logRoute(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  //save original function
+  const original = descriptor.value
+  descriptor.value = function (...args: any[]) {
+    let req = args[0] as Request
+    let res = args[1] as Response
+    //call the original function with arguments that were passed into it
+    original.apply(this, args);
+    //log to console
+    console.log(`${req.ip} [${new Date().toISOString()}] ${req.host} ${req.originalUrl} ${req.method} ${res.statusCode} ${res.statusMessage} HTTP/${req.httpVersion}`);
+    //if you are using PUT OR POST going to output body that was sent in
+    if (["PUT", "POST"].indexOf(req.method) > -1) {
+      console.log(`\tBODY: ${JSON.stringify(req.body)}`)
+    }
+  }
+
+}

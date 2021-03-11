36836578c1018704b0256d230467690f55d8c153
James Lovejoy <james.lovejoy2@gmail.com>
2021-03-11 06:25:23 -0600

    added validation decorators as well as validation function logic

---
+++ decorators/validation.ts
@@ -0,0 +1,129 @@
+import validator from "validator";
+import "reflect-metadata";
+import { mainModule } from "node:process";
+
+type ValidationFunction = (target: any, propertyKey: string, validatorOptions?: any) => string | void;
+
+interface ValidationRule {
+  validationOptions?: any;
+  validator: ValidationFunction;
+}
+
+//decorator that calls addVAlidation method
+export function isEmail(target: any, propertyKey: string) {
+  addValidation(target, propertyKey, emailValidator);
+}
+
+export function required(target: any, propertyKey: string) {
+  addValidation(target, propertyKey, requiredValidator)
+}
+
+//decorator factory for this one
+// advantage this allows us to pass in min and max parameters to be passed in as options
+export function length(min: Number, max: Number) {
+  const options = {
+    min: min,
+    max: max,
+  }
+  return function (target: any, propertyKey: string) {
+    addValidation(target, propertyKey, lengthValidator, options)
+  }
+}
+
+export function isPhone(target: any, propertyKey: string) {
+  addValidation(target, propertyKey, phoneValidator)
+}
+
+export function isInteger(min: Number, max: Number) {
+  const options = {
+    min: min,
+    max: max
+  }
+  return function (target: any, propertyKey: string) {
+    addValidation(target, propertyKey, integerValidator, options)
+  }
+}
+
+function addValidation(target: any, propertyKey: string, validator: ValidationFunction, validationOptions?: any) {
+  // Grab all the properties for the object
+  // if property key is not in properties metaData, add it
+  let objectProperties: string[] = Reflect.getMetadata("validation:properties", target) || [];
+  if (!objectProperties.includes(propertyKey)) {
+    objectProperties.push(propertyKey)
+    Reflect.defineMetadata("validation:properties", objectProperties, target);
+  }
+
+  // get list of validation rules
+  let validators: ValidationRule[] = Reflect.getMetadata("validation:rules", target, propertyKey)
+  // make new rule based on what was passed in
+  let validationRule = {
+    validator: validator,
+    validationOptions: validationOptions
+  };
+  //add the new validation rule
+  validators.push(validationRule);
+  Reflect.defineMetadata("validation:rules", validators, target, propertyKey)
+}
+
+//___________________
+// VALIDATOR FUNCTIONS
+//___________________
+
+function emailValidator(target: any, propertyKey: string): string | void {
+  let value = target[propertyKey]
+  if (value == null) {
+    return;
+  }
+  const isValid = validator.isEmail(value);
+  if (!isValid) {
+    return `Property ${propertyKey} must be a valid email.`
+  }
+  return;
+}
+
+function requiredValidator(target: any, propertyKey: string): string | void {
+  let value = target[propertyKey]
+  if (value) {
+    return;
+  }
+  return `Property ${propertyKey} is required.`
+}
+
+function integerValidator(target: any, propertyKey: string, validatorOptions: any): string | void {
+  const value = target[propertyKey]
+  if (value == null) {
+    return;
+  }
+  const errorMessage = `Property ${propertyKey} must be an integer between ${validatorOptions.min} and ${validatorOptions.max}`
+  if (!Number.isInteger(value)) {
+    return errorMessage
+  }
+  if (value <= validatorOptions.max && value >= validatorOptions.min) {
+    return;
+  }
+  return errorMessage;
+}
+
+function lengthValidator(target: any, propertyKey: string, validatorOptions: any): string | void {
+  const options = {
+    min: validatorOptions.min,
+    max: validatorOptions.max
+  }
+  const isValid = validator.isLength(target[propertyKey] + '', options);
+  if (!isValid) {
+    return `Property ${propertyKey} must be a string between ${validatorOptions.min} and ${validatorOptions.max}`
+  }
+  return;
+}
+
+function phoneValidator(target: any, propertyKey: string, validatorOptions: any): string | void {
+  let value = target[propertyKey]
+  if (value == null) {
+    return;
+  }
+  const isValid = validator.isMobilePhone(value);
+  if (!isValid) {
+    return `Property ${propertyKey} must be a valid phone number`
+  }
+  return;
+}
